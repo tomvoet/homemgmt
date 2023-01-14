e05cb43892fa1f99a0ed35ab425ae30e0095e7bb
Tom Voet <tomvoet@gmx.de>
2023-01-15 00:20:42 +0100

    Added basic auth functionality

--- app/src/main/java/com/example/hce_rfid_cardemulatorsample/MainActivity.kt
+++ app/src/main/java/com/example/hce_rfid_cardemulatorsample/MainActivity.kt
@@ -10,9 +10,13 @@ import android.util.Log
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import androidx.biometric.BiometricPrompt
 import com.example.hce_rfid_cardemulatorsample.databinding.ActivityMainBinding
+import java.util.concurrent.Executor
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +25,62 @@ class MainActivity : AppCompatActivity() {
     private lateinit var hostApduServiceIntent: Intent
     private var messageObserver : Observer<String>? = null
 
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        executor = ContextCompat.getMainExecutor(this)
+
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Log in to Unlock Door")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+
+
+
         hostApduServiceIntent = Intent(this@MainActivity, MyHostApduService::class.java)
+
         checkIfDeviceCanEmulateHostNFCTag()
         binding.editTextApduResponse.setText(Utils.APDURESPONSE)
         setupApduTextChangedListener()
         setMessageObserver()
+
+        binding.authFAB.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
     }
 
     override fun onResume() {
@@ -102,6 +153,11 @@ class MainActivity : AppCompatActivity() {
         } else {
             binding.emulating.text = "Your device can NOT emulate NFC cards"
         }
+    }
+
+    private fun onAuthClick() {
+        Toast.makeText(applicationContext, "Authentication Successful!", Toast.LENGTH_SHORT).show()
+
     }
 
 }
